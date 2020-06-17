5076eacff783928d83f4c3da6d5df5db188cbcf2
Crayonjk <jikaigg@outlook.com>
2020-06-17 23:37:29 +0800

    first day

---
+++ TheOne/choud-provider-payment8001/src/main/java/com/jikaig/springcloud/PaymentMain8001.java
@@ -0,0 +1,8 @@
+package com.jikaig.springcloud;
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}

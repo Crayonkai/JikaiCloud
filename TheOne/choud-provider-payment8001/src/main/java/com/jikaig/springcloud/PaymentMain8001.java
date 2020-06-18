be5c977a3f2a2eecf7237621dafb22bdd26ab9f2
jikaigggg <jikaigg@gmail.com>
2020-06-18 11:09:37 +0800

    create thetwo

--- TheOne/choud-provider-payment8001/src/main/java/com/jikaig/springcloud/PaymentMain8001.java
+++
@@ -1,8 +0,0 @@
-package com.jikaig.springcloud;
-
-@SpringBootApplication
-public class PaymentMain8001 {
-    public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
-    }
-}
---
+++ TheTwo/cloud-provider-payment8001/src/test/java/com/jikaigg/cloud/test.java
@@ -0,0 +1,14 @@
+package com.jikaigg.cloud;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class test {
+
+    @Test
+    public void test1(){
+
+    }
+}

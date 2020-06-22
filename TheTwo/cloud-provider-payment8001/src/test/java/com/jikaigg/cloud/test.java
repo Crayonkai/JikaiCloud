f217f4dcd66d3baf8d15aa7275cf6e51f7aee031
jikaigggg <jikaigg@gmail.com>
2020-06-22 18:56:45 +0800

    test 1111

--- TheTwo/cloud-provider-payment8001/src/test/java/com/jikaigg/cloud/test.java
+++ TheTwo/cloud-provider-payment8001/src/test/java/com/jikaigg/cloud/test.java
@@ -4,11 +4,16 @@ package com.jikaigg.cloud;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.Random;
+
 @Slf4j
 public class test {
-
     @Test
     public void test1(){
-
+        Random rand = new Random(47);
+        log.info("rand is : {}",rand);
+        double a = 2.12;
+        double b = +a;
+        log.info("b is : {}",b);
     }
 }
